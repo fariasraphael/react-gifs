c92f81c3a11475fa946e87da26514cfd37b00ce8
Raphael Farias <farias.raphael@gmail.com>
2024-01-22 22:42:26 -0300

    update app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 
 import SearchBar from './search_bar.jsx'
+import Gif from './gif.jsx'
+import GifList from './gif_list.jsx'
 
 class App extends Component {
   render() {
+    const gifs = [
+        {id: 'jnQYWZ0T4mkhCmkzcn'},
+        {id: '5rcwnHg6PCwQPNp0bi'}
+      ];
+
     return (
       <div>
         <div className="left-scene">
           <SearchBar />
+          <div className="selected-gif">
+            <Gif id="jnQYWZ0T4mkhCmkzcn"/>
+          </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs={gifs} />
+        </div>
       </div>
     );
   }
