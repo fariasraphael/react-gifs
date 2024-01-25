feeabb01f9a405bd63c362532ee5650b59149c49
Raphael Farias <farias.raphael@gmail.com>
2024-01-24 21:47:19 -0300

    add giphy api serach method

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import giphy from 'giphy-api';
 
-import SearchBar from './search_bar.jsx'
-import Gif from './gif.jsx'
-import GifList from './gif_list.jsx'
+import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs:[],
+      selectedGifId: 'jnQYWZ0T4mkhCmkzcn'
+    }
+
+    this.search("sad baby")
+  }
+
+  search = (query) => {
+    giphy('p9wpXw9vFVlSUAE4rdzwCOGLkKp2Zd4X').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+    });
+  }
+
   render() {
     const gifs = [
         {id: 'jnQYWZ0T4mkhCmkzcn'},
@@ -16,11 +40,11 @@ class App extends Component {
         <div className="left-scene">
           <SearchBar />
           <div className="selected-gif">
-            <Gif id="jnQYWZ0T4mkhCmkzcn"/>
+            <Gif id={this.state.selectedGifId}/>
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs={gifs} />
+          <GifList gifs={this.state.gifs} />
         </div>
       </div>
     );
