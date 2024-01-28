ffbe01b3f141f72edab3443a04f7768ac70ca466
Raphael Farias <farias.raphael@gmail.com>
2024-01-28 13:19:38 -0300

    update gif component

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 class Gif extends Component {
+	handleClick = () => {
+    	if (this.props.gifClickedGifList) {
+      		this.props.gifClickedGifList(this.props.id);
+      		console.log("find me at gif")
+    	}
+  	}
+
 	render() {
 		const src = `https://media2.giphy.com/media/${this.props.id}/200.gif`
 		return (
-			<img src={src} alt="" className="gif"/>
+			<img src={src} alt="" className="gif" id={this.props.id} onClick={this.handleClick}/>
 		);
  	}
 }
