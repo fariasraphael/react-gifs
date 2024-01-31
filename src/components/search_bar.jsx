3cb6ef55f13fdd0ca1c80a43e42357792e623253
Raphael Farias <farias.raphael@gmail.com>
2024-01-30 22:49:09 -0300

    fix searchbar component

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 
-	handleUpdate = () => {
+	handleUpdate = (event) => {
+		//console.log(event.target.value)
 		const {searchFunction} = this.props;
 		searchFunction(event.target.value);
 	}
 
 	render() {
 		return (
-			<input type="text" className="form-control form-search"  name="search-bar"
-			onChange={this.handleUpdate}/>
+			<input type="text" className="form-control form-search" name="search-bar" onChange={this.handleUpdate}/>
 		);
  	}
 }
